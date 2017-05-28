71490bb953dd07776074bbdc08e7ad7b240c3a16
Sergey Bezugliy <s.bezugliy@gmail.com>
2017-05-28 21:45:06 +0300

    modified:   mix.exs

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CouchDB.Mixfile do
 
   def project do
     [app: :couchdb_adapter,
-     version: "0.1.0-alpha.0",
+     version: "0.1.1-alpha.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
